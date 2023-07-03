1f78aec4ea3da77571e4f8969baf77ec3e524af6
Omar Sood <omar.sood@external.telekom.de>
2023-07-03 10:01:53 +0200

    feat: initial backend sources

---
+++ backend/src/main/java/de/holossupply/samples/vuewebfluxclientrouter/webfluxbff/WebfluxBffApplication.java
@@ -0,0 +1,13 @@
+package de.holossupply.samples.vuewebfluxclientrouter.webfluxbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxBffApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/holossupply/samples/vuewebfluxclientrouter/webfluxbff/WebfluxBffApplicationTests.java
@@ -0,0 +1,13 @@
+package de.holossupply.samples.vuewebfluxclientrouter.webfluxbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
